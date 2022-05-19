09b8e3fc019ff917b91bb531af8f0957dee652ae
moise <mbakopmoise237@gmail.com>
2022-05-19 18:39:59 +0800

    working on the styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./dist/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        '3xl': 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+      }
+    },
   },
   plugins: [],
 }
