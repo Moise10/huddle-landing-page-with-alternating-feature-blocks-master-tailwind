2235e46646b20e9aaed2743529dd8f0ccc82bd9d
moise <mbakopmoise237@gmail.com>
2022-05-24 11:50:20 +0800

    working on the styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     extend: {
       boxShadow:{
         '3xl': 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+      },
+      backgroundImage:{
+        'desktop':' url(./dist/images/bg-hero-desktop.svg)'
       }
     },
   },
